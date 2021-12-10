00c3222af374766da883f3c145fb443de733bac5
Enrico03Muller <91874302+Enrico03Muller@users.noreply.github.com>
2021-12-11 00:34:04 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,17 @@
+//
+// Created by Enrico Muller on 11/12/2021.
+//
 
+#include "lib.h"
+
+float massimo (float numeri[], int n){
+    float max;
+    max = numeri [0];
+
+    for (int i = 0; i < n; i++){
+        if (max < numeri [i]){
+            max = numeri [i];
+        }
+    }
+    return max;
+}
