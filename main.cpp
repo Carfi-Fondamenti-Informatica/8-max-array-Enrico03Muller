3fa1e6a2c5ae09b2b425942472cc26e2434ca93d
Enrico03Muller <91874302+Enrico03Muller@users.noreply.github.com>
2021-12-11 00:35:00 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
+#include "lib.h"
 
-int main(){
-  return 0;
+using namespace std;
+
+int main () {
+    int n = 0;
+    cout << "Inserisci la dimensione dell'array: ";
+    cin >> n;
+
+    float numeri[n];
+    for (int i = 0; i < n; i++) {
+        cout << "Inserisci il valore " << i << " dell'array: ";
+        cin >> numeri[i];
+    }
+
+    cout << massimo (numeri, n) << endl;
+    return 0;
 }
