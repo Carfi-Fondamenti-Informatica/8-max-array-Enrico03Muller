3239220b0a3f85daddbb51f04a4d3f89b6fd0946
Enrico03Muller <91874302+Enrico03Muller@users.noreply.github.com>
2021-12-11 01:04:15 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -2,7 +2,7 @@
 // Created by Enrico Muller on 11/12/2021.
 //
 
-#ifndef MAIN_CPP_LIB_H
-#define MAIN_CPP_LIB_H
+#ifndef ESERCIZIO8_0_0_LIB_H
+#define ESERCIZIO8_0_0_LIB_H
 float massimo (float, int);
-#endif //MAIN_CPP_LIB_H
+#endif //ESERCIZIO8_0_0_LIB_H
