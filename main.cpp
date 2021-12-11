93416ec27d11987584eb4c8d97e410157a74e613
Enrico03Muller <91874302+Enrico03Muller@users.noreply.github.com>
2021-12-11 01:32:20 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,12 +5,12 @@ using namespace std;
 
 int main () {
     int n = 0;
-    cout << "Inserisci la dimensione dell'array: ";
+    //cout << "Inserisci la dimensione dell'array: ";
     cin >> n;
 
     float numeri[n];
     for (int i = 0; i < n; i++) {
-        cout << "Inserisci il valore " << i << " dell'array: ";
+        //cout << "Inserisci il valore " << i << " dell'array: ";
         cin >> numeri[i];
     }
 
