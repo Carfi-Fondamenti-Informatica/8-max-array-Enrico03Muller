bbca6e4b4589c0767c9aa7033c58adfa665f451c
Enrico03Muller <91874302+Enrico03Muller@users.noreply.github.com>
2021-12-11 01:38:44 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,15 +5,15 @@ using namespace std;
 
 int main () {
     int n = 0;
-    //cout << "Inserisci la dimensione dell'array: ";
+    cout << "Inserisci la dimensione dell'array: ";
     cin >> n;
 
     float numeri[n];
     for (int i = 0; i < n; i++) {
-        //cout << "Inserisci il valore " << i << " dell'array: ";
+        cout << "Inserisci il valore " << i << " dell'array: ";
         cin >> numeri[i];
     }
-
-    cout << massimo (numeri, n) << endl;
+    cout << test(4,5);
+    cout << check (numeri, n) << endl;
     return 0;
 }
